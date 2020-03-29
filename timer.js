551d025316a5282f48d10e54ca74518056f9fa19
luohao <luohao937@gmail.com>
2020-03-29 14:18:37 +0700

    timer completed

---
+++ timer.js
@@ -0,0 +1,48 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callBacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
+    if (callBacks) {
+      this.onStart = callBacks.onStart;
+      this.onTick = callBacks.onTick;
+      this.onComplete = callBacks.onComplete;
+    }
+  }
+
+  start = () => {
+    if (this.onStart) {
+      this.onStart(this.timeRemaining);
+    }
+    this.tick();
+    this.intervalId = setInterval(this.tick, 20);
+  };
+
+  pause = () => {
+    clearInterval(this.intervalId);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      this.timeRemaining = this.timeRemaining - 0.02;
+      if (this.onTick) {
+        this.onTick(this.timeRemaining);
+      }
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time.toFixed(2);
+  }
+}
